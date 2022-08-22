50f31a67936584e64e8c18be35304d4be26e97ce
Guillermo <gcasti_17@yahoo.com.ar>
2022-08-22 20:48:49 -0300

    Correcciones en script de simulación

--- simulation/GTKWave/test_MasterSPI.sh
+++ simulation/GTKWave/test_MasterSPI.sh
@@ -1,5 +1,6 @@
-ghdl -a --ieee=synopsys ../../sources/MasterSPI.vhd ../../sources/shift_reg_sipo.vhd ../../sources/shift_reg_piso.vhd ../../sources/genTimeOut.vhd ../../sources/register_N.vhd ../../sources/MasterSPI_tb.vhd 
-ghdl -s MasterSPI.vhd MasterSPI.vhd
+ghdl -a --ieee=synopsys ../../sources/MasterSPI.vhd ../../sources/shift_reg_sipo.vhd ../../sources/shift_reg_piso.vhd ../../sources/genTimeOut.vhd ../../sources/register_N.vhd ../../sources/counter_N.vhd ../../sources/MasterSPI_tb.vhd 
+ghdl -s --ieee=synopsys ../../sources/MasterSPI.vhd ../../sources/shift_reg_sipo.vhd ../../sources/shift_reg_piso.vhd ../../sources/genTimeOut.vhd ../../sources/register_N.vhd ../../sources/counter_N.vhd ../../sources/MasterSPI_tb.vhd 
+
 ghdl -e MasterSPI_tb
 ghdl -r MasterSPI_tb --vcd=MasterSPI_tb.vcd --stop-time=500ns
 gtkwave MasterSPI_tb.vcd
